bc177375fbb99e7d91c8a6b1921685909d3ba00b
mhmdNoman <mhmdnoman01@gmail.com>
2023-10-04 13:56:28 +0600

    added routes and pages

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,7 +1,22 @@
 const Login = () => {
   return (
-<div>Login</div>
-  )
+    <div className="container mx-auto flex justify-center">
+      <form className="flex justify-center flex-col items-center">
+        <h1 className="mt-8 mb-5 text-center text-2xl">Login with Email and password.</h1>
+        <input
+          type="text"
+          placeholder="Email"
+          className="input input-bordered w-full max-w-xs mb-5"
+        />
+        <input
+          type="text"
+          placeholder="Password"
+          className="input input-bordered w-full max-w-xs mb-5"
+        />
+        <button className="btn btn-neutral">Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -1,6 +1,21 @@
 const Signup = () => {
   return (
-<div>Signup</div>
+    <div className="container mx-auto flex justify-center">
+      <form className="flex justify-center flex-col items-center">
+        <h1 className="mt-8 mb-5 text-center text-2xl">Sign up with Email and password.</h1>
+        <input
+          type="text"
+          placeholder="Email"
+          className="input input-bordered w-full max-w-xs mb-5"
+        />
+        <input
+          type="text"
+          placeholder="Password"
+          className="input input-bordered w-full max-w-xs mb-5"
+        />
+        <button className="btn btn-neutral">Sign up</button>
+      </form>
+    </div>
   )
 };
 
